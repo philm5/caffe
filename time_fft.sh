6b25ebdf00ac846db774ffc48b6f2c45abbd6f6d
Philipp Harzig <philipp@harzig.com>
2016-01-26 15:19:17 +0100

    * deleted some unecessary files

--- time_fft.sh
+++
@@ -1,1 +0,0 @@
-./build/tools/caffe time -model models/bvlc_reference_caffenet/test_fft.prototxt -weights models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel -iterations 10
--- val_fft.sh
+++
@@ -1,2 +0,0 @@
-rm val_fft.txt
-./build/tools/caffe test -model models/bvlc_reference_caffenet/test_fft.prototxt -weights models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel -gpu 0 -iterations 140 &> val_fft.txt
